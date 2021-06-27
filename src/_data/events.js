3e5ac792e4530ddc9daa66b9089997bd7318de78
Sia Karamalegos <siakaramalegos@gmail.com>
2021-06-27 17:24:29 -0500

    Add talk and speaker data to event pages

--- src/_data/events.js
+++ src/_data/events.js
@@ -8,16 +8,53 @@ const urlFor = require('../utils/imageUrl.js')
 // TODO: delete?
 const hasToken = !!client.config().token
 
+function toMarkdown(block) {
+  return BlocksToMarkdown(block, { serializers, ...client.config() })
+}
+
+function generateSpeaker(speaker) {
+  return {
+    ...speaker,
+    bio: toMarkdown(speaker.bio),
+    headshotSrc: urlFor(speaker.image).width(300).height(300).url(),
+  }
+}
+
+function generateTalk(talk) {
+  return {
+    ...talk,
+    description: toMarkdown(talk.description),
+    speaker: talk.speaker ? generateSpeaker(talk.speaker) : null,
+  }
+}
+
 function generateEvent (event) {
   return {
     ...event,
-    overview: BlocksToMarkdown(event.overview, { serializers, ...client.config() }),
-    // headshotSrc: urlFor(event.image).width(300).height(300).url(),
+    overview: toMarkdown(event.overview),
+    talks: event.eventTalks ? event.eventTalks.map(talk => generateTalk(talk)) : null,
   }
 }
 
 async function getEvents () {
-  const filter = groq`*[_type == "event"] | order(beginAt desc)`
+  const filter = groq`
+    *[_type == "event"]{
+      title,
+      overview,
+      startAt,
+      slug,
+      'eventTalks': eventTalks[].talk->{
+        title,
+        description,
+        'speaker': person->{
+          name,
+          bio,
+          image,
+          website,
+          twitter
+        }
+      }
+    }| order(startAt.local desc)`
   const docs = await client.fetch(filter).catch(err => console.error(err))
   const events = docs.map(generateEvent)
   const reducedEvents = overlayDrafts(hasToken, events)
